ce2336253e1080f845c14b7cf81a37c0338948c3
Gleidson <GleidBrianMuniz@gmail.com>
2025-10-20 12:54:36 -0300

    add função de catalogo no streamlit

--- Front-End/app.py
+++ Front-End/app.py
@@ -9,3 +9,16 @@ API_URL = "http://127.0.0.1:8000"
 st.set_page_config(page_title="Gerenciador de Estoque", page_icon="🚛")
 st.title("📦 Gerenciador de Estoque")
 
+#Menu lateral
+menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar produtos", "Atualizar produtos"])
+
+if menu == "Catalogo":
+    st.subheader("Todos os produtos disponiveis")
+    response = requests.get(f"{API_URL}/produto")
+    if response.status_code == 200:
+        produtos = response.json().get("produtos", [])
+        if produtos:
+            st.dataframe(produtos)
+    else:
+        st.error("Erro ao acessar a API")
+
