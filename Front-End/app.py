31474ea5f971255f7cbe79921d5495238bd98c6f
Gleidson <GleidBrianMuniz@gmail.com>
2025-10-20 13:18:26 -0300

    add função de deletar no streamlit

--- Front-End/app.py
+++ Front-End/app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Gerenciador de Estoque", page_icon="🚛")
 st.title("📦 Gerenciador de Estoque")
 
 #Menu lateral
-menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar produto", "Atualizar produto"])
+menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar produto", "Atualizar produto", "Deletar produto"])
 
 if menu == "Catalogo":
     st.subheader("Todos os produtos disponiveis")
@@ -41,7 +41,7 @@ if menu == "Adicionar produto":
 
 
 
-elif menu == "Atualizar produto":
+if menu == "Atualizar produto":
     st.subheader("🔁 Atualizar produtos")
     id_produto = st.number_input("Id do produto", min_value=0, step=1)
 
@@ -67,3 +67,26 @@ elif menu == "Atualizar produto":
             st.error("Erro ao consultar o produto.")
     else:
         st.info("Informe um ID válido para buscar o produto.")
+
+
+if menu == "Deletar produto":
+    st.subheader("❌ Deletar produto")
+    id_produto = st.number_input("Id do produto para deletar", min_value=1, step=1)
+
+    if id_produto > 0:
+        # Consulta o produto para verificar se existe
+        response = requests.get(f"{API_URL}/produto/{id_produto}")
+        if response.status_code == 200:
+            produto = response.json()
+            if produto:
+                st.write(f"Produto: {produto.get('nome')} - Categoria: {produto.get('categoria')} - Preço: R$ {produto.get('preco')} - Quantidade: {produto.get('quantidade')}")
+                if st.button("Deletar Produto"):
+                    response_delete = requests.delete(f"{API_URL}/produto/{id_produto}")
+                    if response_delete.status_code == 200:
+                        st.success("Produto deletado com sucesso!")
+                    else:
+                        st.error("Erro ao deletar o produto.")
+            else:
+                st.warning("Produto não encontrado.")
+        else:
+            st.error("Erro ao consultar o produto.")
