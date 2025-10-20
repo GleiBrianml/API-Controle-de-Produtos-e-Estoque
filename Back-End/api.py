cc6a721e6a48862d0f99079e43ac36680e18af3e
Gleidson <GleidBrianMuniz@gmail.com>
2025-10-20 08:58:32 -0300

    Estrutura de pastas e conexão com o estoque

---
+++ Back-End/api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import funcao
+# python -m uvicorn api:app --reload
+#Testar api Fastapi
+# /docs > Documentação Swagger 
+# /redoc > Documentação
+#inciar o fastapi
+app = FastAPI(title="Gerenciador de Estoque e Produtos")
+
+
